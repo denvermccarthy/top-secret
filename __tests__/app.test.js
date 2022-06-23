879980632d8d5f3bccfab5a98c59a209a320d203
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-22 19:48:45 -0700

    added post route to sign up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,24 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  email: 'test@example.com',
+  name: 'Example Name',
+  password: 'hello world',
+};
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('post to /users should create a user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { name, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+      name,
+    });
   });
   afterAll(() => {
     pool.end();
---
+++ __tests__/users.test.js
@@ -0,0 +1,16 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+// const request = require('supertest');
+// const app = require('../lib/app');
+
+describe('user route testing', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('example test - delete me!', () => {
+    expect(1).toEqual(1);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
