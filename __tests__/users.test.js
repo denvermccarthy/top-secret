eda1533984e75288e436a79634692bd574a78abb
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-22 20:07:38 -0700

    added session route for signing in

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,14 +1,35 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
-
+const request = require('supertest');
+const app = require('../lib/app');
+const mockUser = {
+  email: 'test@example.com',
+  name: 'Example Name',
+  password: 'hello world',
+};
+const signUp = async () => {
+  return await request(app).post('/api/v1/users').send(mockUser);
+};
 describe('user route testing', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('post to /users should create a user', async () => {
+    const res = await signUp();
+    const { name, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+      name,
+    });
+  });
+  test('post to /sessions should sign in an existing user', async () => {
+    await signUp();
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@example.com', password: 'hello world' });
+    expect(res.status).toEqual(200);
   });
   afterAll(() => {
     pool.end();
